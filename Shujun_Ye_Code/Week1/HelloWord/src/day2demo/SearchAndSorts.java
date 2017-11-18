8bfb53f9a405446822bf95e2d95b297b5acda325
Shujun Ye <shujunye@Shujun.local>
2017-11-18 15:56:51 -0500

    Add bubble sort into SearchAndSorts.java

--- Shujun_Ye_Code/Week1/HelloWord/src/day2demo/SearchAndSorts.java
+++ Shujun_Ye_Code/Week1/HelloWord/src/day2demo/SearchAndSorts.java
@@ -51,21 +51,24 @@ public class SearchAndSorts {
 	/**
 	 * bubble sort
 	 */
+	public static void bubbleSort(int[] arr) {
+		// the length of integer array
+		int len = arr.length;
+		for (int i = 0; i < len - 1; i++) {
+			for (int j = 0; j < len - i - 1; j++) {
+				if (arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+	}
 	
 	/**
 	 * merge sort
 	 */
-	// merge sort of arr[]
-	// merge two subarrays, first array is arr[left...middle], second is arr[middle+1...right]
-	void merge(int arr[], int left, int middle, int right) {
-		
-		
-	}
 
-	// sort arr[left...right] using merge
-	void sort(int arr[], int left, int right) {
-		
-	}
 	
 	/**
 	 * insertion sort
