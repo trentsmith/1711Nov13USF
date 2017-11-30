7f95a6f194980b6651e8bdcef71cf8678f0f6fd4
shujunye <shujunye94@gmail.com>
2017-11-30 10:46:47 -0500

    finish 6 JS functions and update homework.html

--- Shujun_Ye_Code/Javascript_Homework/homework.js
+++ Shujun_Ye_Code/Javascript_Homework/homework.js
@@ -32,11 +32,18 @@ function bubbleSort(numArray) {
 			}
 		}
 	}
-	return numArray; // need to fix input
+	return numArray;
 }
 
 function runBub() {
-	var numArray = document.getElementById("bub").value;
+	var numArray =[
+		parseInt(document.getElementById("one").value),
+		parseInt(document.getElementById("two").value),
+		parseInt(document.getElementById("three").value),
+		parseInt(document.getElementById("four").value),
+		parseInt(document.getElementById("five").value)
+	];
+	
 	document.getElementById("bubDisplay").innerHTML = bubbleSort(numArray); // display
 }
 
@@ -68,8 +75,17 @@ document.getElementById("doRev").addEventListener("click", runRev);
  * Define function: factorial(someNum)
  * Use recursion to compute and return the factorial of someNum.
  */
+function factorial(someNum) {
+	if (someNum == 0) return 1;
+	else return (someNum * factorial(someNum - 1));
+}
 
+function runFac() {
+	var n = document.getElementById("fac").value;
+	document.getElementById("facDisplay").innerHTML = factorial(n); // display
+}
 
+document.getElementById("doFac").addEventListener("click", runFac);
 
 /**
  * 5. Substring
@@ -77,7 +93,29 @@ document.getElementById("doRev").addEventListener("click", runRev);
  * Return the substring contained between offset and (offset + length) inclusively.
  * If incorrect input is entered, use the alert function and describe why the input was incorrect.
  */
+function substring(someStr, length, offset){
+	var str = "";
+	for (i = offset; i <= (offset + length); i++){
+		str += someStr.charAt(i);
+	}
+	return str;
+}
 
+function runSub() {
+	var str = document.getElementById("someStr").value;
+	var len = parseInt(document.getElementById("length").value);
+	var offset = parseInt(document.getElementById("offset").value);
+	var truLen = str.length - 1; // the length of str
+	if (offset > truLen || offset < 0){
+		alert("Invalid offset input!!!");
+	} else if ((offset + len) > truLen || len < 0){
+		alert("Invalid length!!!");
+	} else{
+		document.getElementById("subDisplay").innerHTML = substring(str, len, offset); // display
+	}
+}
+
+document.getElementById("doSub").addEventListener("click", runSub);
 
 /**
  * 6. Even Number
@@ -85,4 +123,19 @@ document.getElementById("doRev").addEventListener("click", runRev);
  * Return true if even, false if odd.
  * Do not use % operator. 
  */
+function isEven(someNum){
+	if (someNum == 0) return true;
+	if (Number.isInteger(someNum/2)) return true;
+	return false;
+}
 
+function runEven() {
+	var n = document.getElementById("even").value;
+	if (isEven(n)) {
+		document.getElementById("evenDisplay").innerHTML = n + " is an even number!"; // display
+	} else{
+		document.getElementById("evenDisplay").innerHTML = n + " is an odd number!"; // display
+	}
+}
+
+document.getElementById("doEven").addEventListener("click", runEven);
