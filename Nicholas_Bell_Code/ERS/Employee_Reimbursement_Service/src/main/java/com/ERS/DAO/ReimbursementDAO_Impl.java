20de16a1f66e541a2438ef08c2634d7a043c0cd0
BelliousNick <nick.bell.001@gmail.com>
2017-12-04 10:51:39 -0500

    opdate

---
+++ Nicholas_Bell_Code/ERS/Employee_Reimbursement_Service/src/main/java/com/ERS/DAO/ReimbursementDAO_Impl.java
@@ -0,0 +1,22 @@
+package com.ERS.DAO;
+
+import java.util.ArrayList;
+
+import com.ERS.pojos.Reimbursement;
+import com.ERS.pojos.User;
+
+public class ReimbursementDAO_Impl implements ReimbursementDAO {
+
+	@Override
+	public Reimbursement addReimbursement(User u) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Reimbursement> getAllReimbursements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Nicholas_Bell_Code/ERS/Employee_Reimbursement_Service/src/main/java/com/ERS/service/Service.java
@@ -0,0 +1,5 @@
+package com.ERS.service;
+
+public class Service {
+
+}
