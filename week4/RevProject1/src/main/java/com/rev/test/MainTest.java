b2d09e078eac517d170eb44f976a740c6aef432c
COLLEGE\Andy <adrnix@gmail.com>
2017-12-05 00:00:45 -0500

    Week 4 initial update

---
+++ week4/RevProject1/src/main/java/com/rev/test/MainTest.java
@@ -0,0 +1,14 @@
+package com.rev.test;
+
+import com.rev.DAO.DAO;
+import com.rev.DAO.DAOimp;
+
+public class MainTest {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello ERS");
+		DAO dao = new DAOimp();
+		
+	}
+	
+}
